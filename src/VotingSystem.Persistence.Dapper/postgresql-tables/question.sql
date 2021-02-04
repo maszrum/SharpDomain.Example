19e5052034d130b13418f9e0208c960dd9ec1e16
maszrum <grzyb3k@hotmail.com>
2021-02-04 21:24:34 +0100

    added sql for tables creating

---
+++ src/VotingSystem.Persistence.Dapper/postgresql-tables/question.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE public.question
+(
+    id uuid NOT NULL,
+    question_text character varying(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public.question
+    OWNER to db_user;
