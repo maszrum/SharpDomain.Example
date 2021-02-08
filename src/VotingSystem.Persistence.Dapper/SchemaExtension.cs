94a4bd3d51e636bf983681104c3d8a49e3537857
maszrum <grzyb3k@hotmail.com>
2021-02-08 20:32:17 +0100

    added schema extension

---
+++ src/VotingSystem.Persistence.Dapper/SchemaExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace VotingSystem.Persistence.Dapper
+{
+    internal static class SchemaExtension
+    {
+        public static string InjectSchema(this string sql, string schemaName) => 
+            sql.Replace("@Schema", schemaName);
+    }
+}
