e19cd855c5365b8228025fb2d2262cdd71273427
maszrum <grzyb3k@hotmail.com>
2021-02-08 18:15:12 +0100

    added postgres connection

---
+++ src/VotingSystem.Persistence.Dapper/AutofacExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Npgsql;
+
+namespace VotingSystem.Persistence.Dapper
+{
+    public static class AutofacExtensions
+    {
+        public static ContainerBuilder RegisterDapperPersistence(this ContainerBuilder containerBuilder)
+        {
+            containerBuilder
+                .RegisterGeneric(typeof(DatabaseConnectionBehavior<,>))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+            
+            containerBuilder
+                .Register(context => new NpgsqlConnection()) // TODO: provide connection string
+                .AsSelf()
+                .InstancePerLifetimeScope();
+            
+            containerBuilder
+                .Register(context =>
+                {
+                    var configuration = context.Resolve<DatabaseConfiguration>();
+                    return configuration.Schema;
+                })
+                .As<SchemaProvider>()
+                .SingleInstance();
+            
+            return containerBuilder;
+        }
+    }
+}
---
+++ src/VotingSystem.Persistence.Dapper/DatabaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace VotingSystem.Persistence.Dapper
+{
+    internal delegate string SchemaProvider();
+    
+    internal class DatabaseConfiguration
+    {
+        public string ConnectionString { get; init; } = 
+            "Server=localhost;Port=5432;Database=voting_system;User Id=db_user;Password=db_pass;";
+        
+        public string Schema { get; init; } = 
+            "public";
+    }
+}
