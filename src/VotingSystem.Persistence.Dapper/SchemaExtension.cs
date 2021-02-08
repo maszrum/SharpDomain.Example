3299c82fa4987692c78df47a1a7e8262a9c4cb1c
maszrum <grzyb3k@hotmail.com>
2021-02-08 21:43:37 +0100

    added to snake case converter

--- src/VotingSystem.Persistence.Dapper/SchemaExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VotingSystem.Persistence.Dapper
-{
-    internal static class SchemaExtension
-    {
-        public static string InjectSchema(this string sql, string schemaName) => 
-            sql.Replace("@Schema", schemaName);
-    }
-}
---
+++ src/VotingSystem.Persistence.Dapper/SqlQueryExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace VotingSystem.Persistence.Dapper
+{
+    internal static class SqlQueryExtensions
+    {
+        public static string InjectSchema(this string sql, string schemaName) => 
+            sql.Replace("@Schema", schemaName);
+        
+        public static string PascalCaseToSnakeCase(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(input), "cannot be empty");
+            }
+            
+            var numberOfCapitals = input.Count(char.IsUpper);
+            var resultLength = input.Length + numberOfCapitals - 1;
+            var outArray = new char[resultLength];
+
+            outArray[0] = char.ToLower(input[0]);
+            var outI = 1;
+            for (var i = 1; i < input.Length; i++, outI++)
+            {
+                var character = input[i];
+                if (char.IsUpper(character))
+                {
+                    outArray[outI] = '_';
+                    outArray[outI + 1] = char.ToLower(character);
+                    outI++;
+                }
+                else
+                {
+                    outArray[outI] = character;
+                }
+            }
+            
+            return new string(outArray);
+        }
+    }
+}
