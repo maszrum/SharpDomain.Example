69fde570bb93a5dfb0e957d72d7715dc03b3e0ff
maszrum <grzyb3k@hotmail.com>
2021-02-09 19:36:43 +0100

    compiler error

---
+++ src/VotingSystem.Persistence/IsExternalInit.cs
@@ -0,0 +1,6 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    // compiler bug
+    // https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
+    public class IsExternalInit{}
+}
