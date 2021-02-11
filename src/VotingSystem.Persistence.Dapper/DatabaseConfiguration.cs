2f6fd11735a9dc747c8526147cfe844d534189dc
maszrum <grzyb3k@hotmail.com>
2021-02-11 09:43:09 +0100

    added todo comment

--- src/VotingSystem.Persistence.Dapper/DatabaseConfiguration.cs
+++ src/VotingSystem.Persistence.Dapper/DatabaseConfiguration.cs
@@ -4,6 +4,7 @@
     
     public class DatabaseConfiguration
     {
+        // TODO: remove default values when configuration module will be finished
         public string ConnectionString { get; init; } = 
             "Server=localhost;Port=5432;Database=voting_system;User Id=db_user;Password=db_pass;";
         
